dd50e77db6e478a7c23e6c559c28df8f87606295
TakoFerenc <rockferi@yahoo.com>
2017-09-11 23:13:00 +0200

    Small config change

--- device_config.py
+++ device_config.py
@@ -3,10 +3,10 @@ class Config:
 
     @staticmethod
     def desired_capabilities():
-        desired_caps = [{'deviceName': '42004cd04e8e4400',
+        desired_caps = [{'deviceName': '################',
                         'platformName': 'Android',
                         'platformVersion': '5.1.1',
-                        'app': 'c:\\Users\\Fred\\AndroidProject\\wonky-android-calc-master\\bin\\Calculator.apk',
+                        'app': 'c:\\Users\\Ferenc\\AndroidProject\\wonky-android-calc-master\\bin\\Calculator.apk',
                         'noReset': 'true',
                         'fullReset': 'false',
                         'appPackage': 'com.test.calc',
